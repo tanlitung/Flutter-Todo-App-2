8553d6af0dd22d66fdc9c5de078f3faaadc8bf7b
tanlitung388 <tanlitung388@gmail.com>
2020-03-30 18:45:11 +0800

    Added all files

---
+++ todo_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/pages/home.dart';
+import 'package:todoapp/pages/add_tasks.dart';
+import 'package:todoapp/pages/datepicker.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Home(),
+      '/addTasks': (context) => AddTasks(),
+      '/datepicker': (context) => DatePicker(),
+    }
+  ));
+}
+
